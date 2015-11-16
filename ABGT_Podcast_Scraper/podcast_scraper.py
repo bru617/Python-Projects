ba18290eabd50b34edce4060483b2761d8d5a3cb
bru617 <marshall176@ntlworld.com>
2015-11-16 21:19:19 +0000

    Initial Commit

---
+++ ABGT_Podcast_Scraper/podcast_scraper.py
@@ -0,0 +1,25 @@
+import feedparser
+import json
+import urllib2
+
+# search term will only return one entry, as being specific (above-beyond-group-therapy) & podcast entity
+BASE_URL = "https://itunes.apple.com/search?term=above-beyond-group-therapy&country=GB&entity=podcast"
+
+# load JSON data from base url into a dict obj
+abgt_dict = json.load(urllib2.urlopen(BASE_URL))
+
+if abgt_dict['resultCount'] == 1 :
+    # knowing abgt search is one entry returned, ok to use [0]
+    feedUrl = abgt_dict['results'][0]['feedUrl']
+    artworkUrl30 = abgt_dict['results'][0]['artworkUrl30']
+    artworkUrl60 = abgt_dict['results'][0]['artworkUrl60']
+    artworkUrl100 = abgt_dict['results'][0]['artworkUrl100']
+
+# parse RSS feed URL
+feed = feedparser.parse(feedUrl)
+ent_list = feed['entries']
+for a in ent_list:
+    print a['links'][1]['href']
+
+
+
